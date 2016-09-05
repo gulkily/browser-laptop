d37d7371bcc3df6e59f8eefcb03309048fb95a5f
Brian Clifton <brian@clifton.me>
2016-09-05 16:17:12 -0700

    Reworked styles for caption buttons

--- js/components/main.js
+++ js/components/main.js
@@ -38,7 +38,7 @@ const ContextMenu = require('./contextMenu')
 const PopupWindow = require('./popupWindow')
 const NoScriptInfo = require('./noScriptInfo')
 const LongPressButton = require('./longPressButton')
-const WindowActionBar = require('./windowActionBar')
+const WindowCaptionButtons = require('./windowCaptionButtons')
 
 // Constants
 const config = require('../constants/config')
@@ -864,8 +864,8 @@ class Main extends ImmutableComponent {
                 braveShieldsDown: !braverySettings.shieldsUp
               })}
               onClick={this.onBraveMenu} />
-              { isWindows ? <WindowActionBar windowMaximized={this.props.windowState.getIn(['ui', 'isMaximized'])} /> : null }
           </div>
+          { isWindows ? <WindowCaptionButtons windowMaximized={this.props.windowState.getIn(['ui', 'isMaximized'])} /> : null }
         </div>
         <UpdateBar updates={this.props.appState.get('updates')} />
         {
